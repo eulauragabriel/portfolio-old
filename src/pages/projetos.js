cc4110356f41a23b0d92e7c941749298c393dcef
Laura Gabriel <eulauragabriel@gmail.com>
2024-06-25 08:23:51 -0300

    Adicionando soft e hard skills

--- src/pages/projetos.js
+++ src/pages/projetos.js
@@ -19,34 +19,150 @@ const Projetos = () => {
         <a href="https://github.com/eulauragabriel/site-unes" target="_blank">
           <img src="/images/projetoI.png" alt="site unes" width="30%" />
           <h4>UNES: Um site educacional com recursos interativos desenvolvido por mim para a aula de Desenvolvimento Web I.</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>MySQL</li>
+              <li>Docker</li>
+              <li>Python</li>
+            </ul>
+          </h4>
+          <h4>soft skills:
+            <ul>
+              <li>Autonomia</li>
+            </ul>
+          </h4>
         </a>
         <a href="https://github.com/eulauragabriel/portfolio" target="_blank">
           <img src="/images/projetoII.png" alt="site portfolio" width="30%" />
           <h4>Portfólio: Uma coleção pessoal de trabalhos e ilustrações desenvolvido por mim para a aula de Design Digital.</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+            </ul>
+          </h4>
+          <h4>soft skills:
+            <ul>
+              <li>Autonomia</li>
+              <li>Estimativa de prazo</li>
+            </ul>
+          </h4>
         </a>
         <a href="https://github.com/Equipe-CodeLand/API-2023.1" target="_blank">
           <img src="/images/projetoIII.png" alt="site api 1° semestre" width="30%" />
           <h4>API 1° Semestre: Um site desenvolvido pela minha equipe sobre Covid Longa no qual participei do time de desenvolvimento.</h4>
+        <h4>hard skills:
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>ChartJS</li>
+            <li>Python</li>
+            <li>Flask</li>
+            <li>MySQL</li>
+          </ul>
+        </h4>
+        <h4>soft skills:
+          <ul>
+            <li>Comunicação</li>
+            <li>Trabalho em equipe</li>
+            <li>Empatia</li>
+            <li>Autonomia</li>
+          </ul>
+        </h4>
         </a>
       </div>
       <div className={styles.images}>
         <a href="https://github.com/Equipe-CodeLand/API-2023.2" target="_blank">
           <img src="/images/projetoIV.png" alt="site api 2° semestre" width="30%" />
           <h4>API 2° Semestre: Um site de Gerenciamento de Chamadas desenvolvido pela minha equipe no qual participei do time de desenvolvimento.</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+              <li>TypeORM</li>
+              <li>MySQL</li>
+              <li>React</li>
+              <li>NodeJS</li>
+            </ul>
+          </h4>
+          <h4>soft skills:
+            <ul>
+              <li>Comunicação</li>
+              <li>Trabalho em equipe</li>
+              <li>Empatia</li>
+              <li>Autonomia</li>
+            </ul>
+          </h4>
         </a>
         <a href="https://github.com/leticiacarvalho04/WB/tree/atvi5-wb" target="_blank">
           <img src="/images/projetoV.png" alt="site wb" width="30%" />
           <h4>Projeto WB: Um site destinado à uma empresa fictícia World Beauty desenvolvido por mim e por uma colega para a aula de Tecnicas de Programação I</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+            </ul>
+          soft skills:
+            <ul>
+              <li>Comunicação</li>
+              <li>Trabalho em equipe</li>
+              <li>Autonomia</li>
+            </ul>
+          </h4>
         </a>
         <a href="https://github.com/eulauragabriel/desenvolvimento-web-II/tree/main/lista_03" target="_blank">
           <img src="/images/projetoVI.png" alt="site lista03" width="30%" />
           <h4>Crud de Músicas: Um crud desenvolvido por mim para aula de Desenvolvimento Web II.</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>NodeJS</li>
+              <li>Express</li>
+              <li>MySQL</li>
+            </ul>
+          </h4>
+          <h4>soft skills:
+            <ul>
+              <li>Autonomia</li>
+              <li>Estimativa de prazo</li>
+            </ul>
+          </h4>
         </a>
       </div>
       <div className={styles.images}>
         <a href="https://github.com/Equipe-CodeLand/API-2024.1" target="_blank">
           <img src="/images/projetoVII.png" alt="site api 2024.1" width="30%" />
           <h4>API 2024.1: Um site desenvolvido pela minha equipe sobre um sistema de Gerenciamento de Ativos para a empresa YouTan no qual participei do time de desenvolvimento.</h4>
+          <h4>hard skills:
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>React</li>
+              <li>NodeJS</li>
+              <li>MySQL</li>
+            </ul>
+          </h4>
+          <h4>soft skills:
+            <ul>
+              <li>Comunicação</li>
+              <li>Trabalho em equipe</li>
+              <li>Empatia</li>
+              <li>Autonomia</li>
+              <li>Estimativa de prazo</li>
+              <li>Comunicação com cliente externo</li>
+            </ul>
+          </h4>
         </a>
         <a href=""></a>
         <a href=""></a>
